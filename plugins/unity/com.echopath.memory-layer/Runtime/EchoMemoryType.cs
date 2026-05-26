9bca3e8062359d39653c16d1826bc11b32528308
EchoPath XR <echopathxr@gmail.com>
2026-05-26 18:26:00 -0400

    Add Unity memory type enum

---
+++ plugins/unity/com.echopath.memory-layer/Runtime/EchoMemoryType.cs
@@ -0,0 +1,13 @@
+namespace EchoPath.MemoryLayer
+{
+    public enum EchoMemoryType
+    {
+        Presence,
+        Hiding,
+        Sound,
+        Danger,
+        Safe,
+        Route,
+        Custom
+    }
+}
